e5756192036915fb78e85d53e3cb81cca538c67b
KrishnaRaj <Krishna.Devarajan-Nair@saiglobal.com>
2018-06-07 14:28:52 +1000

    Initial commit

---
+++ src/pages/about/about.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+
+  }
+  closeModal(){
+    this.viewCtrl.dismiss();
+  }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { NavController, LoadingController, ModalController } from 'ionic-angular';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { AboutPage } from '../about/about';
+
+
+interface INews {
+  articles: any
+}
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  serachQuery: string = '';
+  news: any;
+  constructor(public navCtrl: NavController, private httpClient: HttpClient, public loadingCtrl: LoadingController, public modalCtrl: ModalController) {
+    this.getNews();
+  }
+
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: 'Please wait...',
+      duration: 3000,
+      dismissOnPageChange: true
+    });
+    loader.present();
+  }
+
+  openModal() {
+    const myModal = this.modalCtrl.create(AboutPage);
+    myModal.present();
+  }
+
+  getNews() {
+    this.httpClient.get<INews>('http://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=364a811e3dfc495b9cdfb4f4a3e70112').
+      subscribe(data => {
+        this.presentLoading();
+        this.news = data.articles;
+      },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log('Client-side error occured');
+          }
+          else {
+            console.log('Server-side error occured');
+          }
+        }
+      );
+  }
+
+  getNewsbyFilter(ev: any) {
+    const val = ev.target.value;
+    if (val && val.trim() != '') {
+      this.news = this.news.filter((item) => {
+        return (item.toLowercase().indexOf(val.toLowercase()) > -1);
+      })
+    }
+  }
+}
